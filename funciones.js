c296a9ea35a2170b5154d4098669a5987064aa56
AlexisRoman1148 <61295180+AlexisRoman1148@users.noreply.github.com>
2020-02-25 13:57:09 -0600

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,16 @@
+function cambiarcolor() {
+    document.body.style.backgroundColor = 'black';
+    document.getElementById('text').style.color = 'white';
+    document.getElementById('cabecera').style.backgroundColor = '#0B0B61';
+    document.getElementById('pie').style.backgroundColor = '#0B0B61';
+    document.getElementById('derechos').style.color = 'white';
+    document.getElementById('links').style.color = 'white';
+}
+
+function normal() {
+    document.body.style.backgroundColor = 'white';
+    document.getElementById('text').style.color = 'black';
+    document.getElementById('cabecera').style.backgroundColor = 'cyan';
+    document.getElementById('pie').style.backgroundColor = 'cyan';
+    document.getElementById('derechos').style.color = 'black';
+}
